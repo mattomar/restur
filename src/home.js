aa35a0eeb31d751f4cd4b1afca71f08a1f09104a
mattomar <moddathero@gmail.com>
2023-09-01 14:50:13 +0700

    did the restur deatils-home section

--- src/home.js
+++ src/home.js
@@ -2,52 +2,48 @@ export function createRestaurantPage() {
     document.addEventListener('DOMContentLoaded', () => {
         const contentDiv = document.getElementById('content');
 
-        // Create and append the ul list to homeTabs
-        const ul = document.createElement('ul');
-
-        const homeTab = document.createElement('li');
-        homeTab.textContent = 'Home';
-        ul.appendChild(homeTab);
-
-        const whyFoodTab = document.createElement('li');
-        whyFoodTab.textContent = 'Why Food';
-        ul.appendChild(whyFoodTab);
-
-        const menuTab = document.createElement('li');
-        menuTab.textContent = 'Menu';
-        ul.appendChild(menuTab);
-
-        const locationTab = document.createElement('li');
-        locationTab.textContent = 'Location';
-        ul.appendChild(locationTab);
-
-        // Create a div for homeTabs and append the ul
-        const homeTabsDiv = document.createElement('div');
-        homeTabsDiv.id = 'homeTabs';
-        homeTabsDiv.appendChild(ul);
-
-        contentDiv.appendChild(homeTabsDiv);
+        
 
         // Create a div for homeBody
         const homeBody = document.createElement('div');
         homeBody.id = 'homeBody';
 
-        const restaurantImageContainer = document.createElement('div');
-        restaurantImageContainer.classList.add('restaurant-image-container'); // Add a class for styling
-        
-        homeBody.appendChild(restaurantImageContainer);
+      
 
-        // Create and append the restaurant heading
+        const restaurantDetails = document.createElement('div');
+        restaurantDetails.classList.add('restaurant-details');
+
         const restaurantHeading = document.createElement('h1');
-        restaurantHeading.textContent = 'Our Wonderful Restaurant';
-        homeBody.appendChild(restaurantHeading);
+        restaurantHeading.classList.add('restaurant-heading'); 
+        restaurantHeading.innerHTML = 'The   Tastiest &<br><span class="best-pizza">Best Pizza</span> In The <br>World'; // Use innerHTML for line breaks
+        restaurantDetails.appendChild(restaurantHeading);
 
-        // Create and append the restaurant description
         const restaurantDescription = document.createElement('p');
-        restaurantDescription.textContent = 'Welcome to our restaurant! We serve delicious food in a cozy atmosphere.';
-        homeBody.appendChild(restaurantDescription);
+        restaurantDescription.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br>sed do eiusmod tempor incididunt ut labore .<br>Ut enim ad minim veniam, quis nostrud exercitation.';
+        restaurantDescription.classList.add('restaurant-description'); // Add the "restaurant-description" class
+        restaurantDetails.appendChild(restaurantDescription);
+
+        
+        homeBody.appendChild(restaurantDetails);
+
+
+
+        // Create and append the view menu button
+        const viewMenuButton = document.createElement('button');
+        viewMenuButton.textContent = 'View Menu';
+        viewMenuButton.id = 'viewMenuButton'; // Add an ID for styling or interaction
+        restaurantDetails.appendChild(viewMenuButton);
+
+        // Append the button container to homeBody
+        homeBody.appendChild(restaurantDetails);
 
         // Append homeBody to contentDiv
         contentDiv.appendChild(homeBody);
+
+        const restaurantImageContainer = document.createElement('div');
+        restaurantImageContainer.classList.add('restaurant-image-container'); // Add a class for styling
+
+        homeBody.appendChild(restaurantImageContainer);
     });
 }
+
